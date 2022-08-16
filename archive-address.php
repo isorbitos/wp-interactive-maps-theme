28f93b5bf7faa46eed458c1547a0fa8e1cda8e76
DESKTOP-CFOMFKS\User <marius.pasiulevicius@gmail.com>
2022-08-16 10:19:57 +0300

    creating child theme for astra

---
+++ archive-address.php
@@ -0,0 +1,18 @@
+<?php 
+get_header();
+?>
+<div class="">
+    <?php 
+    while(have_posts()){
+        the_post();?>
+        <h1><a href="<?php the_permalink() ?>"> <?php the_title(); ?></a></h1>
+    <?php }
+    
+    ?>
+
+</div>
+
+
+<?php
+get_footer();
+?>
---
+++ single-address.php
@@ -0,0 +1,19 @@
+<?php 
+get_header();
+?>
+<div class="">
+    <?php 
+    while(have_posts()){
+        the_post();?>
+        <h1><?php the_title(); ?></h1>
+
+    <?php }
+    
+    ?>
+
+</div>
+
+
+<?php
+get_footer();
+?>
